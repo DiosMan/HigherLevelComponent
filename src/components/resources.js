c5045008a66516de574decb01d197553df0bc6a6
Dios <David@Daweis-MacBook-Pro.local>
2017-01-08 16:51:30 -0800

    First commit

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default () => {
+    return (
+        <div>
+            Super Special Recipe
+            <ul>
+                <li>1 suger</li>
+                <li>1 pepeer</li>
+                <li>1 beer</li>
+            </ul>
+        </div>
+    );
+}
